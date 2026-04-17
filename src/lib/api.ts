90fcccbd42f89f2c3b9afa0dbc1a3eb8adc8b79e
Leonardo1596 <leonardoemanuel156@gmail.com>
2026-04-17 02:12:11 -0300

    changed route

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,2 +1,2 @@
-// export const API_BASE_URL = 'https://financial-control-442c.onrender.com';
-export const API_BASE_URL = 'http://192.168.0.38:8000';
+export const API_BASE_URL = 'https://financial-control-442c.onrender.com';
+// export const API_BASE_URL = 'http://192.168.0.38:8000';
